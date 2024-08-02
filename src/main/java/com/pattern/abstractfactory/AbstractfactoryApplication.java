675b0033026f047a349342d78b0b763374d01ead
99nirmit <79360867+99nirmit@users.noreply.github.com>
2024-08-02 13:46:26 +0530

    Shape Project

--- src/main/java/com/pattern/abstractfactory/AbstractfactoryApplication.java
+++ src/main/java/com/pattern/abstractfactory/AbstractfactoryApplication.java
@@ -1,5 +1,7 @@
 package com.pattern.abstractfactory;
 
+import domain.Shape;
+import domain.ShapeFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AbstractfactoryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AbstractfactoryApplication.class, args);
+
+//		Shape Problem
+
+		Shape shape1 = ShapeFactory.getShape("CIRCLE");
+		shape1.draw();
+
+		Shape shape2 = ShapeFactory.getShape("RECTANGLE");
+		shape2.draw();
 	}
 
 }
---
+++ src/main/java/domain/Rectangle.java
@@ -0,0 +1,10 @@
+package domain;
+
+public class Rectangle implements Shape{
+
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+}
